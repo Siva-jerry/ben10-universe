3daa385d95b5de88dd053f1eb0a8436d03e6fa90
Siva-jerry <msiva0127@gmail.com>
2026-06-24 10:24:10 +0530

    Added GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/ben10-universe/',
   plugins: [react()],
 })
